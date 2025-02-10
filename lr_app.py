a60ab9d8ee40ec3b33b2916cfe098d547f28650a
Dhe-Reigner <martinkagema01@gmail.com>
2025-02-10 17:57:24 +0300

    additional commit

--- lr_app.py
+++ lr_app.py
@@ -8,3 +8,11 @@ model  =pickle.load(open('linear_regression_model.pkl', 'rb'))
 
 # let's create web app
 st.title("Scikit-Learn Linear Regression Model")
+tv = st.text_input("Enter Tv sales...")
+radio = st.text_input("Enter radio sales...")
+newspaper = st.text_input("Enter newspaper sales...")
+
+if st.button("predict"):
+    features = np.array([[tv, radio, newspaper]], dtype=np.float64)
+    results = model.predict(features).reshape(1, -1)
+    st.write("Predicted sale::::", results[0])
